423095fc8d972eee13bee15dd1d18f521f82e1d9
steve-conrad <steve.conrad14@gmail.com>
2025-07-13 14:27:10 -0400

    finished basic button layout

--- pyprmenu/pyprmenu.py
+++ pyprmenu/pyprmenu.py
@@ -7,37 +7,38 @@ class PyprMenu(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.geometry = ("400x250")
+        self.geometry = ("900x600")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
         #Main Menu
         self.main_menu_frame = ctk.CTkFrame(self)
         self.main_menu_frame.grid(row=0, column=0, sticky="nsew")
-        self.main_menu_frame.grid_rowconfigure(0, weight=1)
+        self.main_menu_frame.grid_rowconfigure(1, weight=1)
+        self.main_menu_frame.grid_columnconfigure(0, weight=1)
+        self.main_menu_frame.grid_columnconfigure(1, weight=1)
+        self.main_menu_frame.grid_columnconfigure(2, weight=1)
         
         self.button_1 = ctk.CTkButton(
-            self.main_menu_frame, text="Shutdown",
+            self.main_menu_frame, 
+            text="Shutdown",
             command=self.button_shutdown,
-            height=50
         )
-        self.button_1.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+        self.button_1.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
         self.button_2 = ctk.CTkButton(
             self.main_menu_frame,
             text="Restart",
             command=self.button_restart,
-            height=50
         )
-        self.button_2.grid(row=1, column=1, padx=10, pady=10, sticky="ew")
+        self.button_2.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
 
         self.button_3 = ctk.CTkButton(
             self.main_menu_frame,
             text="Lock",
             command=self.button_lock,
-            height=50
         )
-        self.button_3.grid(row=1, column=2, padx=10, pady=10, sticky="ew")
+        self.button_3.grid(row=1, column=2, padx=10, pady=10, sticky="nsew")
 
 # Define button actions
     def button_shutdown(self):
