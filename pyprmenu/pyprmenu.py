984e47e39d98e7f20b55d4510b7a75c8005eb8d0
steve-conrad <steve.conrad14@gmail.com>
2025-07-13 15:45:25 -0400

    fixed fonts and added additional commands

--- pyprmenu/pyprmenu.py
+++ pyprmenu/pyprmenu.py
@@ -26,7 +26,7 @@ class PyprMenu(ctk.CTk):
             fg_color="#1f1f28",
             hover_color="#2d2d40",
             text_color="#bdf6ec",
-            font=("JetBrainsMono", 28, "bold"),
+            font=("JetBrainsMono Nerd Font", 28, "bold"),
         )
         self.shutdown.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
@@ -37,7 +37,7 @@ class PyprMenu(ctk.CTk):
             fg_color="#1f1f28",
             hover_color="#2d2d40",
             text_color="#bdf6ec",
-            font=("JetBrainsMono", 28, "bold"),
+            font=("JetBrainsMono Nerd Font", 28, "bold"),
         )
         self.restart.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
 
@@ -48,16 +48,16 @@ class PyprMenu(ctk.CTk):
             fg_color="#1f1f28",
             hover_color="#2d2d40",
             text_color="#bdf6ec",
-            font=("JetBrainsMono", 28, "bold"),
+            font=("JetBrainsMono Nerd Font", 28, "bold"),
         )
         self.lock.grid(row=1, column=2, padx=10, pady=10, sticky="nsew")
 
 # Define button actions
     def button_shutdown(self):
-        print("Shutdown")
+        subprocess.run(["systemctl", "poweroff"])
 
     def button_restart(self):
-        print("Restart")
+        subprocess.run(["systemctl", "reboot"])
 
     def button_lock(self):
         subprocess.run(["hyprlock"])
